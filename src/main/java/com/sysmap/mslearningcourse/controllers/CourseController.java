0e7e6936935e055c7fbd51b56a269a270611f3b6
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-02-16 19:05:18 -0300

    Created get course method

--- src/main/java/com/sysmap/mslearningcourse/controllers/CourseController.java
+++ src/main/java/com/sysmap/mslearningcourse/controllers/CourseController.java
@@ -1,10 +1,10 @@
 package com.sysmap.mslearningcourse.controllers;
 
+import com.sysmap.mslearningcourse.entities.Course;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/course")
@@ -14,6 +14,14 @@ public class CourseController {
     public ResponseEntity<String> createCourse(
         @RequestBody
         String courseName
+    ) {
+        return null;
+    }
+
+    @GetMapping("/{courseId}")
+    public ResponseEntity<Course> getcourse(
+        @PathVariable
+        UUID courseId
     ) {
         return null;
     }
