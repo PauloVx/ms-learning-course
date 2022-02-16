e1e5b77011d13e3f2b701d6f088a280c18520516
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-02-16 16:59:40 -0300

    Created Course Model

--- src/main/java/com/sysmap/mslearningcourse/entities/Course.java
+++ src/main/java/com/sysmap/mslearningcourse/entities/Course.java
@@ -1,8 +1,37 @@
 package com.sysmap.mslearningcourse.entities;
 
-import lombok.Data;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
-
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@Document
 public class Course {
+
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private UUID courseId;
+
+    private String courseName;
+    private Boolean status;
+    private LocalDateTime createdOn;
+
+    public Course(
+        UUID courseId,
+        String courseName,
+        Boolean status,
+        LocalDateTime createdOn
+    ) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.status = status;
+        this.createdOn = createdOn;
+    }
 }
