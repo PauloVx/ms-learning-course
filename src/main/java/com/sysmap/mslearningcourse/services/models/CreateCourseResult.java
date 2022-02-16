353d865a53292b8dba25b99a017413cc241871ab
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-02-16 19:23:55 -0300

    Added all args constructor to create course result

--- src/main/java/com/sysmap/mslearningcourse/services/models/CreateCourseResult.java
+++ src/main/java/com/sysmap/mslearningcourse/services/models/CreateCourseResult.java
@@ -1,10 +1,12 @@
 package com.sysmap.mslearningcourse.services.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
+@AllArgsConstructor
 public class CreateCourseResult {
     private UUID courseId;
 }
