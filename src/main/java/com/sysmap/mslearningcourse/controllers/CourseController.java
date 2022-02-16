5c6a0af1441c5d6dad268bb9ff9c4f436b7a2ccb
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-02-16 19:03:14 -0300

    Created create course method

--- src/main/java/com/sysmap/mslearningcourse/controllers/CourseController.java
+++ src/main/java/com/sysmap/mslearningcourse/controllers/CourseController.java
@@ -1,4 +1,20 @@
 package com.sysmap.mslearningcourse.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/course")
 public class CourseController {
+
+    @PostMapping
+    public ResponseEntity<String> createCourse(
+        @RequestBody
+        String courseName
+    ) {
+        return null;
+    }
 }
