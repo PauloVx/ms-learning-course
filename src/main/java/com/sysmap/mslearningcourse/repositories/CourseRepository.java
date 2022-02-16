5287158aa881b363b051885a354be9a541dcd76d
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-02-16 18:49:05 -0300

    Update CourseRepository.java

--- src/main/java/com/sysmap/mslearningcourse/repositories/CourseRepository.java
+++ src/main/java/com/sysmap/mslearningcourse/repositories/CourseRepository.java
@@ -4,8 +4,11 @@ import com.sysmap.mslearningcourse.entities.Course;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CourseRepository extends MongoRepository<Course, UUID> {
+
+    Optional<Course> findCourseByCourseId(UUID courseId);
 }
