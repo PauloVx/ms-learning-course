610ffa5625f8991cc3fa1a958717af0a8ccd01ab
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-02-16 20:01:58 -0300

    Renamed getcourseresult class

--- src/main/java/com/sysmap/mslearningcourse/services/models/GetCourseResult.java
+++ src/main/java/com/sysmap/mslearningcourse/services/models/GetCourseResult.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
-public class GetCoursesResult {
+public class GetCourseResult {
     private UUID courseId;
     private String courseName;
     private Boolean status;
