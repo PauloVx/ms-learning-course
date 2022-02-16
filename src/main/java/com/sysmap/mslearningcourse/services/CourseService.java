7839c4a0ccbccf5d6739e9c413d822f72576ac52
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-02-16 19:24:36 -0300

    Created method to save new course inside course service

--- src/main/java/com/sysmap/mslearningcourse/services/CourseService.java
+++ src/main/java/com/sysmap/mslearningcourse/services/CourseService.java
@@ -1,7 +1,35 @@
 package com.sysmap.mslearningcourse.services;
 
+import com.sysmap.mslearningcourse.controllers.models.CreateCourseInput;
+import com.sysmap.mslearningcourse.entities.Course;
+import com.sysmap.mslearningcourse.repositories.CourseRepository;
+import com.sysmap.mslearningcourse.services.models.CreateCourseResult;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 public class CourseService {
+
+    private CourseRepository courseRepository;
+
+    public CourseService(
+        CourseRepository courseRepository
+    ) {
+        this.courseRepository = courseRepository;
+    }
+
+    public CreateCourseResult createCourse(CreateCourseInput courseInput) {
+        var course = new Course(
+            UUID.randomUUID(),
+            courseInput.getCourseName(),
+            true,
+            LocalDateTime.now()
+        );
+
+        var dbResult = this.courseRepository.insert(course);
+
+        return new CreateCourseResult(dbResult.getCourseId());
+    }
 }
